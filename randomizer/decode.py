df146b04e6807474a1cc497ba77c321ecedadc59
Msgames79 <102644909+Msgame79@users.noreply.github.com>
2025-08-26 04:03:06 +0900

    やばそうなやつを追加

--- randomizer/decode.py
+++ randomizer/decode.py
@@ -6,6 +6,7 @@ compiled with pyinstaller
 
 import os
 import math
+import random
 
 def clear_screen():
     if os.name == 'nt':
@@ -14,17 +15,21 @@ def clear_screen():
         os.system('clear')
 
 while True:
-    levels = ["Mansion", "Train", "Carry", "Mountain", "Demolition", "Castle", "Water", "Power Plant", "Aztec", "Dark", "Steam", "Ice", "Reprise"]
+    levels = ["Mansion","Train","Carry","Mountain","Demolition","Castle","Water","Power Plant","Aztec","Dark","Steam","Ice","Reprise"]
+    # levels = ["Mansion","Train","Carry","Mountain","Demolition","Castle","Water","Power Plant","Aztec","Dark","Steam","Ice","Reprise","Thermal","Factory","Golf","City","Forest","Laboratory","Lumber","Red Rock","Tower","Miniature","Copper World","Port","Underwater","Dockyard","Museum","Hike","Candyland","Test Chamber"] # 勇気があればコメントアウト解除
     while True:
         try:
             clear_screen()
-            a = input("Insert a positive integer:")
-            if int(a) >= 1 and int(a) < math.factorial(13):
+            a = input("Insert an integer between 0 and "+str(math.factorial(len(levels))-1)+" otherwise generate a mnumber:")
+            if 0 <= int(a) and int(a) <= math.factorial(len(levels))-1:
                 break
             else:
                 pass
         except ValueError:
-            pass
+            a = random.randint(0,math.factorial(len(levels))-1)
+            clear_screen()
+            print("Insert an integer between 0 and "+str(math.factorial(len(levels))-1)+" otherwise generate a mnumber:"+str(a))
+            break
 
     b=int(a)
     for i in range(len(levels)-1,-1,-1):
